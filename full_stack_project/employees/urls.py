30c0f479f9e804bd464e7746404ff95c4a721094
Tal Rozalis <talrozalis77@gmail.com>
2021-10-23 02:08:24 +0300

    commit_3

--- full_stack_project/employees/urls.py
+++ full_stack_project/employees/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path, include
+<<<<<<< HEAD
 
 urlpatterns = [
 
+=======
+from . import views
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register('employees-api', views.EmployeeView)
+
+urlpatterns = [
+path('employees-api', include(router.urls))
+>>>>>>> b481b53 (commit_3)
 ]
