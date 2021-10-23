49048ad9f5398c915727f5f2028290063c66c1cb
Tal Rozalis <talrozalis77@gmail.com>
2021-10-23 21:33:35 +0300

    commit_5

--- full_stack_project/employees/serializers.py
+++ full_stack_project/employees/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
-from .models import Employee
+from .models import Employee, Title, Specialist, Manager, Employee_ID
+
+class Employee_IDSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee_ID
+        fields = ['id', 'url', 'employee_id']
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ['first_name', 'last_name', 'employee_id', 'email_address', 'phone_number', 'hire_date', 'salary']
+        fields = ['id', 'url', 'first_name', 'last_name', 'employee_id', 'email_address', 'phone_number', 'hire_date', 'salary', 'title']
+
+class TitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Title
+        fields = ['id', 'url', 'title', 'specialist']
+
+class SpecialistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Specialist
+        fields = ['id', 'url', 'specialist']
+
+class ManagerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manager
+        fields = ['id', 'url', 'manager_details', 'direct_reporter_id']
+
--- full_stack_project/employees/urls.py
+++ full_stack_project/employees/urls.py
@@ -2,7 +2,11 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 router = routers.DefaultRouter()
-router.register('employees-api', views.EmployeeView)
+router.register('employees', views.EmployeeView),
+router.register('title', views.TitleView),
+router.register('specialist', views.SpecialistView),
+router.register('manager', views.ManagerView),
+router.register('employee_id', views.Employee_IDView)
 
 urlpatterns = [
 path('employees-api', include(router.urls))
