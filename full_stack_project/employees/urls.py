9d68185fae75b568f89dcb545c255a0a90873571
Tal Rozalis <talrozalis77@gmail.com>
2021-10-24 21:20:38 +0300

    commit_8

--- full_stack_project/employees/urls.py
+++ full_stack_project/employees/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+app_name = 'employees'
+
+
 router = routers.DefaultRouter()
 router.register('employees', views.EmployeeView),
 router.register('title', views.TitleView),
